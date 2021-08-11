263c1e9b5b7cf076e67d939c7e76eeb58b2bb876
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-08-12 03:27:08 +0700

    geography models refactoring

--- src/main/java/com/nyash/travellizermono/api/entity/geography/Coordinate.java
+++ src/main/java/com/nyash/travellizermono/api/entity/geography/Coordinate.java
@@ -11,24 +11,4 @@ public class Coordinate {
 
     private double y;
 
-    public Coordinate(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
 }
