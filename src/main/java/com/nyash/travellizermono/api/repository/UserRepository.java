fd0651c9b2d31273f92591bd487c293bdd3963a1
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-08-13 04:40:27 +0700

    add users fetching feature

--- src/main/java/com/nyash/travellizermono/api/repository/UserRepository.java
+++ src/main/java/com/nyash/travellizermono/api/repository/UserRepository.java
@@ -2,8 +2,10 @@ package com.nyash.travellizermono.api.repository;
 
 import com.nyash.travellizermono.api.entity.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -21,4 +23,11 @@ public interface UserRepository extends JpaRepository <UserEntity, Long> {
      * @return
      */
     Optional<UserEntity> findByUserName(@Param("userName") String userName);
+
+    @Query("SELECT u FROM UserEntity u " +
+            "WHERE :isFiltered = FALSE " +
+            "OR (LOWER(u.firstName) LIKE LOWER(CONCAT('%', :filter, '%'))" +
+            "OR LOWER(u.lastName) LIKE LOWER(CONCAT('%', :filter, '%')))" +
+            "ORDER BY u.lastName, u.firstName")
+    List<UserEntity> findAllByFilter(boolean isFiltered, String filter);
 }
