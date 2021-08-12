a65128e426332b6cb728bd5ff778e3628900f5ff
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-08-13 01:08:12 +0700

    -add user model and user roles

---
+++ src/main/java/com/nyash/travellizermono/api/entity/user/Role.java
@@ -0,0 +1,12 @@
+package com.nyash.travellizermono.api.entity.user;
+
+/**
+ * User role that affects permissions of the current user
+ * @author Nyash
+ *
+ */
+public enum Role {
+    USER,
+    MANAGER,
+    ADMIN
+}
