7daea8a9bbca182f33ab7dccf9fa211383800d3c
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-08-16 10:56:10 +0700

    add fetch station method to GeographicController

--- src/main/java/com/nyash/travellizermono/api/controller/GeographicController.java
+++ src/main/java/com/nyash/travellizermono/api/controller/GeographicController.java
@@ -2,8 +2,11 @@ package com.nyash.travellizermono.api.controller;
 
 import com.nyash.travellizermono.api.common.infra.util.StringChecker;
 import com.nyash.travellizermono.api.dto.CityDTO;
+import com.nyash.travellizermono.api.dto.StationDTO;
 import com.nyash.travellizermono.api.entity.geography.CityEntity;
+import com.nyash.travellizermono.api.entity.geography.StationEntity;
 import com.nyash.travellizermono.api.factory.CityDTOFactory;
+import com.nyash.travellizermono.api.factory.StationDTOFactory;
 import com.nyash.travellizermono.api.repository.CityRepository;
 import com.nyash.travellizermono.api.repository.StationRepository;
 import lombok.AccessLevel;
@@ -13,6 +16,7 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.transaction.Transactional;
@@ -31,7 +35,10 @@ public class GeographicController {
 
     CityDTOFactory cityDtoFactory;
 
+    StationDTOFactory stationDtoFactory;
+
     public static final String FETCH_CITIES = "api/cities";
+    public static final String FETCH_STATIONS = "api/cities/{cityId}/stations";
 
     @GetMapping(FETCH_CITIES)
     public ResponseEntity<List<CityDTO>> fetchCities (
@@ -42,5 +49,17 @@ public class GeographicController {
         List<CityEntity> cities = cityRepository.findAllByFilter(isFiltered, filter);
 
         return ResponseEntity.ok(cityDtoFactory.createCityDTOList(cities));
+    }
+
+    @GetMapping(FETCH_STATIONS)
+    public ResponseEntity<List<StationDTO>> fetchStations(
+            @PathVariable Long cityId,
+            @RequestParam(defaultValue = "")String filter) {
+
+        boolean isFiltered = !filter.trim().isEmpty();
+
+        List<StationEntity> stations = stationRepository.findAllByFilter(isFiltered, filter);
+
+        return ResponseEntity.ok(stationDtoFactory.createStationDTOList(stations));
     }
 }
