a803bb0767afe05838b43355671bde7b74ee55a4
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-08-16 10:02:09 +0700

    add fetchCities method to CityController

--- src/main/java/com/nyash/travellizermono/api/controller/GeographicController.java
+++ src/main/java/com/nyash/travellizermono/api/controller/GeographicController.java
@@ -2,6 +2,8 @@ package com.nyash.travellizermono.api.controller;
 
 import com.nyash.travellizermono.api.common.infra.util.StringChecker;
 import com.nyash.travellizermono.api.dto.CityDTO;
+import com.nyash.travellizermono.api.entity.geography.CityEntity;
+import com.nyash.travellizermono.api.factory.CityDTOFactory;
 import com.nyash.travellizermono.api.repository.CityRepository;
 import com.nyash.travellizermono.api.repository.StationRepository;
 import lombok.AccessLevel;
@@ -11,6 +13,7 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -26,10 +29,18 @@ public class GeographicController {
 
     StationRepository stationRepository;
 
+    CityDTOFactory cityDtoFactory;
+
     public static final String FETCH_CITIES = "api/cities";
 
     @GetMapping(FETCH_CITIES)
-    public ResponseEntity<List<CityDTO>> fetchCities () {
-        return null;
+    public ResponseEntity<List<CityDTO>> fetchCities (
+            @RequestParam(defaultValue = "") String filter) {
+
+        boolean isFiltered = !filter.trim().isEmpty();
+
+        List<CityEntity> cities = cityRepository.findAllByFilter(isFiltered, filter);
+
+        return ResponseEntity.ok(cityDtoFactory.createCityDTOList(cities));
     }
 }
